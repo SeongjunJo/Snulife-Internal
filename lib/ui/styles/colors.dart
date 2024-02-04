ecbb99f259dacd0d21337b89f7d1c4ba3fe3fa4b
SeongjunJo <gukcvts679@snu.ac.kr>
2024-02-05 00:27:49 +0900

    색상 설정
    * SLDS 색상 추가

---
+++ lib/ui/styles/colors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  final Color slBlue = const Color(0xFF03B8F1);
+
+  final Color blue0 = const Color(0xFFF1FBFE);
+  final Color blue1 = const Color(0xFFD3F3FD);
+  final Color blue2 = const Color(0xFFB2EAFB);
+  final Color blue3 = const Color(0xFF8BDFF9);
+  final Color blue4 = const Color(0xFF5DD2F6);
+  final Color blue5 = const Color(0xFF000000);
+  final Color blue6 = const Color(0xFF03ACE0);
+  final Color blue7 = const Color(0xFF0294C1);
+  final Color blue8 = const Color(0xFF027598);
+  final Color blue9 = const Color(0xFF027598);
+
+  final Color grey1 = const Color(0xFFF8F9FA);
+  final Color grey0 = const Color(0xFFF1F3F5);
+  final Color grey2 = const Color(0xFFE9EDF0);
+  final Color grey3 = const Color(0xFFE3E7EB);
+  final Color grey4 = const Color(0xFFCED4DA);
+  final Color grey5 = const Color(0xFFADB5BD);
+  final Color grey6 = const Color(0xFF868E96);
+  final Color grey7 = const Color(0xFF495057);
+  final Color grey8 = const Color(0xFF343A40);
+  final Color grey9 = const Color(0xFF212529);
+
+  final Color white = Colors.white;
+  final Color black = Colors.black;
+}
